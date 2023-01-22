a555286333d7dbb55ef46b87dc696a1e9a22dea8
Nearlan <Arlanzhumag@gmail.com>
2023-01-22 22:57:46 +0600

    Second try

---
+++ Lab_1/Python_syntax/Ex1.py
@@ -0,0 +1,6 @@
+if 5 > 2:
+    print("5 is greater than 2")
+"""
+if 5 > 2:
+print("5 is greater than") --- incorrect
+"""
---
+++ Lab_1/Python_syntax/Ex2.py
@@ -0,0 +1,4 @@
+a = 2 #int variable
+b = "Hello" #str variable
+print(a)
+print(b)
