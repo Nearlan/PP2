7cb4ee1f344aad7a5c9e64403f56e0dab5b3fe71
Nearlan <123264009+Nearlan@users.noreply.github.com>
2023-02-02 22:47:35 +0600

    Lab 2

---
+++ Lab_2/Python_Loops/For/ex10.py
@@ -0,0 +1,7 @@
+for x in range(6):
+  if x == 3: break
+  print(x)
+else:
+  print("Finally finished!")
+
+#If the loop breaks, the else block is not executed.
---
+++ Lab_2/Python_Loops/For/ex6.py
@@ -0,0 +1,5 @@
+for x in range(6):
+  print(x)
+  
+  
+# The range() function returns a sequence of numbers, starting from 0 by default, and increments by 1 (by default), and ends at a specified number.
